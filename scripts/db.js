85b9e0bf92948002e43dba759524bf058306bc25
Vladislav Kraemer <v21kraemer@gmail.com>
2025-01-23 00:05:35 +0100

    Update db.js

---
+++ scripts/db.js
@@ -0,0 +1,34 @@
+let myDishes = [
+  {
+    id: 0,
+    name: "Pizza Margherita",
+    description: "mit Mozzarella",
+    price: 8.5,
+    img: "./assets/img/margherita.jpg",
+    amount: 0,
+  },
+  {
+    id: 1,
+    name: "Pizza Diavolo",
+    description: "mit Salami, Zwiebeln und Knoblauch",
+    price: 10.5,
+    img: "./assets/img/diavolo.jpg",
+    amount: 0,
+  },
+  {
+    id: 2,
+    name: "Pizza Salami",
+    description: "mit Salami und Mais",
+    price: 11.0,
+    img: "./assets/img/pizza-salami.jpg",
+    amount: 0,
+  },
+  {
+    id: 3,
+    name: "Pizza Prosciutto",
+    description: "mit Schinken",
+    price: 12.5,
+    img: "./assets/img/prosciutto.jpg",
+    amount: 0,
+  },
+];
